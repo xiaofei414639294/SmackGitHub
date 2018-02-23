95f29f6fdff7bf7a6002f6de745371b5c09cb697
xiaofei xie <xiaofeixie@xiaofeis-MacBook-Pro.local>
2018-02-23 16:14:01 -0600

    lesson 4.

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by xiaofei xie on 2/23/18.
+//  Copyright © 2018 xiaofei xie. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by xiaofei xie on 2/23/18.
+//  Copyright © 2018 xiaofei xie. All rights reserved.
+//
+
+import Foundation
+
+
+//Segues
+let TO_LOGIN = "toLogin"
+
+
