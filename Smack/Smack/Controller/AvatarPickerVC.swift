4f1c99508af93358543751f8d7a78895c352897d
xiaofei xie <xiaofeixie@xiaofeis-MacBook-Pro.local>
2018-03-19 11:54:43 -0500

    lesson 13

---
+++ Smack/Smack/Controller/AvatarPickerVC.swift
@@ -0,0 +1,48 @@
+//
+//  AvatarPickerVC.swift
+//  Smack
+//
+//  Created by xiaofei xie on 3/19/18.
+//  Copyright © 2018 xiaofei xie. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    
+    //Outlets
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            return cell
+        }
+        return AvatarCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+    }
+ 
+
+}
---
+++ Smack/Smack/View/CollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionViewCell.swift
+//  Smack
+//
+//  Created by xiaofei xie on 3/19/18.
+//  Copyright © 2018 xiaofei xie. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
